7d65f40e494de89f908b0705957c18e6843db5a0
GiuDiMax <giuseppedimauro95@gmail.com>
2024-01-03 18:17:42 +0100

    uvicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from syncLast import syncLast
+import uvicorn
 
 app = FastAPI()
 # uvicorn main:app --reload
@@ -7,10 +8,13 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return syncLast
+    return syncLast()
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
+if __name__ == "__main__":
+  uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
