7af297f5a1c3c54f4e96afa0f4cf4f3ee6035392
GiuDiMax <giuseppedimauro95@gmail.com>
2024-01-08 16:57:23 +0100

    threading

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ from flask import Flask, render_template, redirect, flash, send_from_directory, 
 from syncLast import syncLast
 from syncAllRatings import syncAll
 from syncLibrary import getDiff
+from threading import Thread
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
+
     return jsonify(syncLast())
 
 
 @app.route('/library', methods=['POST', 'GET'])
 def library():
-    return jsonify(getDiff())
+    t = Thread(target=getDiff)
+    print("library start")
+    t.start()
+    return "started"
 
 
 @app.route('/ratings', methods=['POST', 'GET'])
