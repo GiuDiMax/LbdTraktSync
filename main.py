329412631f129d42e2a2b3659ba5205993a7d13a
GiuDiMax <giuseppedimauro95@gmail.com>
2024-01-17 16:01:43 +0100

    edit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, flash, send_from_directory, make_response, send_file, jsonify
 from syncLast import syncLast
-from syncAllRatings import syncAll
-from syncLibrary import getDiff
-from threading import Thread
+#from syncAllRatings import syncAll
+#from syncLibrary import getDiff
+#from threading import Thread
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def home():
     return jsonify(syncLast())
 
-
+"""
 @app.route('/library', methods=['POST', 'GET'])
 def library():
     t = Thread(target=getDiff)
@@ -23,7 +23,7 @@ def library():
 @app.route('/ratings', methods=['POST', 'GET'])
 def ratings():
     return jsonify(syncAll())
-
+"""
 
 if __name__ == '__main__':
     app.run()
